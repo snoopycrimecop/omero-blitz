b55711bcfc2539fd3d484a9169e9ed6b75b9b1df
jmoore <josh@glencoesoftware.com>
2011-03-31 14:32:53 +0200

    Moving repo package to blitz/

---
+++ src/ome/services/blitz/repo/RepoRawFileStoreI.java
@@ -0,0 +1,191 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2010 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+
+package ome.services.blitz.repo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+import ome.services.blitz.impl.AbstractAmdServant;
+import omero.ServerError;
+import omero.api.AMD_RawFileStore_exists;
+import omero.api.AMD_RawFileStore_read;
+import omero.api.AMD_RawFileStore_save;
+import omero.api.AMD_RawFileStore_setFileId;
+import omero.api.AMD_RawFileStore_size;
+import omero.api.AMD_RawFileStore_truncate;
+import omero.api.AMD_RawFileStore_write;
+import omero.api._RawFileStoreOperations;
+import omero.util.IceMapper;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import Ice.Current;
+
+/**
+ * An implementation of the RepoRawFileStore interface
+ *
+ * @author Josh Moore, josh at glencoesoftware.com
+ */
+public class RepoRawFileStoreI extends AbstractAmdServant implements
+_RawFileStoreOperations {
+
+    private final static Log log = LogFactory.getLog(RepoRawFileStoreI.class);
+
+    private final long fileId;
+
+    private final File file;
+
+    private final RandomAccessFile rafile;
+
+    public RepoRawFileStoreI(long fileId, File file) {
+        super(null, null);
+        this.fileId = fileId;
+        this.file = file;
+        try {
+            this.rafile = new RandomAccessFile(file, "r");
+            log.info("Opened " + rafile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void setFileId_async(AMD_RawFileStore_setFileId __cb, long fileId,
+            Current __current) throws ServerError {
+        omero.ApiUsageException aue = new omero.ApiUsageException();
+        aue.message = "Cannot reset id from " + this.fileId + " to " + fileId;
+        __cb.ice_exception(aue);
+    }
+
+    public void exists_async(AMD_RawFileStore_exists __cb, Current __current)
+            throws ServerError {
+        try {
+            __cb.ice_response(file.exists());
+        } catch (Exception e) {
+            __cb.ice_exception(e);
+        }
+    }
+
+    public void read_async(AMD_RawFileStore_read __cb, long position,
+            int length, Current __current) throws ServerError {
+
+        if (length > 64 * 1000 * 1000) {
+            __cb.ice_exception(new omero.ApiUsageException(null, null,
+                    "Too big: " + length));
+            return; // EARLY EXIT!
+        }
+
+        try {
+            final byte[] array = new byte[length];
+            final ByteBuffer buffer = ByteBuffer.wrap(array);
+            this.rafile.getChannel().read(buffer, position);
+            __cb.ice_response(array);
+        } catch (Throwable t) {
+            __cb.ice_exception(convert(t));
+        }
+
+    }
+
+
+    public void size_async(AMD_RawFileStore_size __cb, Current __current)
+            throws ServerError {
+        try {
+            long size = this.rafile.getChannel().size();
+            __cb.ice_response(size);
+        } catch (Throwable t) {
+            __cb.ice_exception(convert(t));
+        }
+    }
+
+    public void truncate_async(AMD_RawFileStore_truncate __cb, long length,
+            Current __current) throws ServerError {
+
+        if (true) {
+            // See ticket:2562
+            __cb.ice_exception(new omero.ApiUsageException(null, null, "Currently disabled."));
+            return;
+        }
+
+        try {
+            FileChannel fc = this.rafile.getChannel();
+            if (fc.size() < length) {
+                __cb.ice_response(false);
+            } else {
+                this.rafile.getChannel().truncate(length);
+                __cb.ice_response(true);
+            }
+        } catch (Throwable t) {
+            __cb.ice_exception(convert(t));
+        }
+    }
+
+    public void write_async(AMD_RawFileStore_write __cb, byte[] buf,
+            long position, int length, Current __current) throws ServerError {
+
+        if (true) {
+            // See ticket:2562
+            __cb.ice_exception(new omero.ApiUsageException(null, null, "Currently disabled."));
+            return;
+        }
+
+
+        ByteBuffer buffer = MappedByteBuffer.wrap(buf);
+        buffer.limit(length);
+
+        try {
+            rafile.getChannel().write(buffer, position);
+            __cb.ice_response();
+        } catch (Throwable t) {
+            __cb.ice_exception(convert(t));
+        }
+
+    }
+
+    public void save_async(AMD_RawFileStore_save __cb, Current __current)
+            throws ServerError {
+        __cb.ice_response(null); // DO NOTHING
+    }
+
+    @Override
+    protected void preClose() {
+        log.info("Closing " + rafile);
+        try {
+            this.rafile.close();
+        } catch (Exception e) {
+            log.error("Failed to close rafile", e);
+        }
+    }
+
+    //
+    // Helpers
+    //
+
+    private omero.ServerError convert(Throwable t) {
+        if (t instanceof omero.ServerError) {
+            return (omero.ServerError) t;
+        }
+
+        omero.ServerError se = null;
+        if (t instanceof IOException) {
+            se = new omero.ResourceError();
+        } else {
+            se = new omero.InternalException();
+        }
+
+        IceMapper.fillServerError(se, t);
+        return se;
+
+    }
+
+}
---
+++ src/ome/services/blitz/repo/ScriptRepositoryI.java
@@ -0,0 +1,61 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2009 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+package ome.services.blitz.repo;
+
+import ome.services.blitz.fire.Registry;
+import ome.services.scripts.ScriptRepoHelper;
+import ome.services.util.Executor;
+import ome.util.SqlAction;
+import omero.ServerError;
+import omero.model.OriginalFile;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import Ice.Current;
+import Ice.ObjectAdapter;
+
+/**
+ * Repository which makes the included script files available to users.
+ *
+ * @since Beta4.2
+ */
+public class ScriptRepositoryI extends AbstractRepositoryI {
+
+    private final static Log log = LogFactory.getLog(ScriptRepositoryI.class);
+
+    private final ScriptRepoHelper helper;
+
+    public ScriptRepositoryI(ObjectAdapter oa, Registry reg, Executor ex, SqlAction sql,
+            String sessionUuid, ScriptRepoHelper helper) {
+        super(oa, reg, ex, sql, sessionUuid, helper.getScriptDir());
+        this.helper = helper;
+    }
+
+    @Override
+    public String generateRepoUuid() {
+        return this.helper.getUuid();
+    }
+
+    /**
+     */
+    public String getFilePath(final OriginalFile file, Current __current)
+            throws ServerError {
+
+        String repo = getFileRepo(file);
+        String uuid = getRepoUuid();
+
+        if (repo == null || !repo.equals(uuid)) {
+            throw new omero.ValidationException(null, null,repo
+                    + " does not belong to this repository: " + uuid);
+        }
+
+        return file.getPath() == null ? null : file.getPath().getValue();
+
+    }
+
+}
