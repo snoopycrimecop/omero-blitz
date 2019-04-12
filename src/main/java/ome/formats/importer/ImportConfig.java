7fbe570d920d44b48f5dccffc1be2345d15ccb95
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-04-12 11:42:42 +0100

    update doc

--- src/main/java/ome/formats/importer/ImportConfig.java
+++ src/main/java/ome/formats/importer/ImportConfig.java
@@ -90,7 +90,7 @@ public class ImportConfig {
     private final Properties props;
 
     /**
-     * Stores the omeroVersion from omero.properties
+     * Stores the omeroVersion from omero-server.properties
      */
     private String omeroVersion = "Unknown";
 
